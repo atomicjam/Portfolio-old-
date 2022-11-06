b7d0ec58de76294e3f554f9464a1008d08bff27d
Elliot Goode <atomicjam@gmail.com>
2022-11-06 12:39:15 +0000

    moved three into project

--- themes/EG/assets/js/bundle.js
+++ themes/EG/assets/js/bundle.js
@@ -1,12 +1,15 @@
 import Alpine from 'alpinejs'
 import intersect from '@alpinejs/intersect'
 
+/* alpine if needed */
 window.Alpine = Alpine
 Alpine.plugin(intersect)
 Alpine.start()
 
+/* register service worker */
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', function () {
         navigator.serviceWorker.register('/sw.js');
     });
 }
+
